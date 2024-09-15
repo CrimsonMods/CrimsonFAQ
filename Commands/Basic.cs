a61a920c537650f0b3383ffc3d1305cd7af1f905
SkyCopeland <sky@skycopeland.com>
2024-09-15 19:11:43 -0400

    Filter faq list by user access

--- Commands/Basic.cs
+++ Commands/Basic.cs
@@ -21,6 +21,7 @@ internal class Basic
         { 
             foreach(KeyResponse kp in Plugin.DB.Responses) 
             {
+                if(kp.isAdmin && !Plugin.DB.IsTrusted(ctx.User)) continue;
                 reply += kp.Key + "\n";
             }
         }
